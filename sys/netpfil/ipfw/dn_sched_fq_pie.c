731834a59ed6a8629d96e8d2638e71680d871403
Deol Satish <164171952+deol-satish@users.noreply.github.com>
2024-06-06 20:55:14 +1000

    Modifying FQ-PIE to correct pre-existing errors in the kernel

--- sys/netpfil/ipfw/dn_sched_fq_pie.c
+++ sys/netpfil/ipfw/dn_sched_fq_pie.c
@@ -337,8 +337,7 @@ fq_pie_extract_head(struct fq_pie_flow *q, aqm_time_t *pkt_ts,
 	struct fq_pie_si *si, int getts)
 {
 	struct mbuf *m;
-
-next:	m = q->mq.head;
+	m = q->mq.head;
 	if (m == NULL)
 		return m;
 	q->mq.head = m->m_nextpkt;
@@ -360,13 +359,46 @@ next:	m = q->mq.head;
 			m_tag_delete(m,mtag); 
 		}
 	}
-	if (m->m_pkthdr.rcvif != NULL &&
-	    __predict_false(m_rcvif_restore(m) == NULL)) {
-		m_freem(m);
-		goto next;
-	}
 	return m;
 }
+
+
+
+// __inline static struct mbuf *
+// fq_pie_extract_head(struct fq_pie_flow *q, aqm_time_t *pkt_ts,
+// 	struct fq_pie_si *si, int getts)
+// {
+// 	struct mbuf *m;
+
+// next:	m = q->mq.head;
+// 	if (m == NULL)
+// 		return m;
+// 	q->mq.head = m->m_nextpkt;
+
+// 	fq_update_stats(q, si, -m->m_pkthdr.len, 0);
+
+// 	if (si->main_q.ni.length == 0) /* queue is now idle */
+// 			si->main_q.q_time = V_dn_cfg.curr_time;
+
+// 	if (getts) {
+// 		/* extract packet timestamp*/
+// 		struct m_tag *mtag;
+// 		mtag = m_tag_locate(m, MTAG_ABI_COMPAT, DN_AQM_MTAG_TS, NULL);
+// 		if (mtag == NULL){
+// 			D("PIE timestamp mtag not found!");
+// 			*pkt_ts = 0;
+// 		} else {
+// 			*pkt_ts = *(aqm_time_t *)(mtag + 1);
+// 			m_tag_delete(m,mtag); 
+// 		}
+// 	}
+// 	if (m->m_pkthdr.rcvif != NULL &&
+// 	    __predict_false(m_rcvif_restore(m) == NULL)) {
+// 		m_freem(m);
+// 		goto next;
+// 	}
+// 	return m;
+// }
 
 /*
  * Callout function for drop probability calculation 
