74379fcaf4c26726fe80258904616c40cc578926
Deol Satish <164171952+deol-satish@users.noreply.github.com>
2024-06-09 01:18:55 +1000

    add more debug statements

--- sys/netpfil/ipfw/dn_sched_fq_pie.c
+++ sys/netpfil/ipfw/dn_sched_fq_pie.c
@@ -338,50 +338,56 @@ fq_update_stats(struct fq_pie_flow *q, struct fq_pie_si *si, int len,
 
 }
 
-/*
- * Extract a packet from the head of sub-queue 'q'
- * Return a packet or NULL if the queue is empty.
- * If getts is set, also extract packet's timestamp from mtag.
- */
 __inline static struct mbuf *
 fq_pie_extract_head(struct fq_pie_flow *q, aqm_time_t *pkt_ts,
-	struct fq_pie_si *si, int getts)
+    struct fq_pie_si *si, int getts)
 {
-	printf("Start fq_pie Extract head");
-	struct mbuf *m;
+    printf("Start fq_pie_extract_head\n");
+    struct mbuf *m;
+    struct m_tag *mtag;
 
-next:	m = q->mq.head;
-	if (m == NULL)
-		return m;
-	q->mq.head = m->m_nextpkt;
+next:
+    printf("Inside next label\n");
+    m = q->mq.head;
+    if (m == NULL) {
+        printf("Queue is empty\n");
+        return m;
+    }
+    q->mq.head = m->m_nextpkt;
 
-	fq_update_stats(q, si, -m->m_pkthdr.len, 0);
+    printf("Updating stats\n");
+    fq_update_stats(q, si, -m->m_pkthdr.len, 0);
 
-	if (si->main_q.ni.length == 0) /* queue is now idle */
-			si->main_q.q_time = V_dn_cfg.curr_time;
+    if (si->main_q.ni.length == 0) { /* queue is now idle */
+        printf("Queue is now idle\n");
+        si->main_q.q_time = V_dn_cfg.curr_time;
+    }
 
-	if (getts) {
-		/* extract packet timestamp*/
-		struct m_tag *mtag;
-		mtag = m_tag_locate(m, MTAG_ABI_COMPAT, DN_AQM_MTAG_TS, NULL);
-		if (mtag == NULL){
-			D("PIE timestamp mtag not found!");
-			*pkt_ts = 0;
-		} else {
-			*pkt_ts = *(aqm_time_t *)(mtag + 1);
-			m_tag_delete(m,mtag); 
-		}
-	}
-	printf("before if end fq_pie Extract head");
-	if (m->m_pkthdr.rcvif != NULL &&
-	    __predict_false(m_rcvif_restore(m) == NULL)) {
-		m_freem(m);
-		printf("insside if fq_pie Extract head");
-		goto next;
-	}
-	printf("final end fq_pie Extract head");
-	return m;
+    if (getts) {
+        /* extract packet timestamp */
+        printf("Extracting packet timestamp\n");
+        mtag = m_tag_locate(m, MTAG_ABI_COMPAT, DN_AQM_MTAG_TS, NULL);
+        if (mtag == NULL) {
+            printf("PIE timestamp mtag not found!\n");
+            *pkt_ts = 0;
+        } else {
+            *pkt_ts = *(aqm_time_t *)(mtag + 1);
+            m_tag_delete(m, mtag); 
+        }
+    }
+
+    printf("Checking if m->m_pkthdr.rcvif is not NULL\n");
+    if (m->m_pkthdr.rcvif != NULL &&
+        __predict_false(m_rcvif_restore(m) == NULL)) {
+        printf("m_rcvif_restore returned NULL, freeing mbuf\n");
+        m_freem(m);
+        goto next;
+    }
+
+    printf("Returning mbuf\n");
+    return m;
 }
+
 
 // __inline static struct mbuf *
 // fq_pie_extract_head(struct fq_pie_flow *q, aqm_time_t *pkt_ts,
