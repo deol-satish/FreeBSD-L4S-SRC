1c1b51d4b335ace17ad1f144a1be57854b72253e
Deol Satish <164171952+deol-satish@users.noreply.github.com>
2024-08-28 03:26:53 +1000

    Wrong Variable name for flows variable name in l4s_queue to initilize new flows

--- sys/netpfil/ipfw/dn_sched_l4s.c
+++ sys/netpfil/ipfw/dn_sched_l4s.c
@@ -1096,10 +1096,10 @@ l4s_enqueue(struct dn_sch_inst *_si, struct dn_queue *_q,
 	if (!flows[idx].active) {
 		STAILQ_INSERT_TAIL(&si->newflows, &flows[idx], flowchain);
 		//flows[idx].deficit = param->quantum;
-		if (f->queue_type == L4S_QUEUE)
-			f->deficit = f->wl * param->quantum;
+		if (flows[idx]->queue_type == L4S_QUEUE)
+			flows[idx]->deficit = flows[idx]->wl * param->quantum;
 		else
-			f->deficit = f->wc * param->quantum;
+			flows[idx]->deficit = flows[idx]->wc * param->quantum;
 		fq_activate_pie(&flows[idx]);
 		flows[idx].active = 1;
 	}
