fab917f447d09132e5d3b38b5cec17991e47764e
Deol Satish <164171952+deol-satish@users.noreply.github.com>
2024-12-20 11:11:17 +1100

    Add Packet length to L4S-LLM Data Collection

--- sys/netpfil/ipfw/dn_sched_l4s.c
+++ sys/netpfil/ipfw/dn_sched_l4s.c
@@ -925,11 +925,11 @@ pie_enqueue(struct l4s_flow *q, struct mbuf* m, struct l4s_si *si)
 	}
 
 
-	printf("l4s_ecn_marking-start,%u,%u,%u,%u,%u,%u,%u,%u,%u,%u,%u,%u,%lu,%u,%u,%u,%u,%lu,%lu,%u,%u,%u,%d,end \n",q->queue_type,pprms->qdelay_ref,pprms->tupdate,
+	printf("l4s_ecn_marking-start,%u,%u,%u,%u,%u,%u,%u,%u,%u,%u,%u,%u,%lu,%u,%u,%u,%u,%lu,%lu,%u,%u,%u,%u,%d,end \n",q->queue_type,pprms->qdelay_ref,pprms->tupdate,
 	pprms->max_burst,pprms->max_ecnth,pprms->alpha,pprms->beta,pprms->flags,
 	pst->burst_allowance,pst->drop_prob,pst->current_qdelay,pst->qdelay_old,pst->accu_prob,
 	pst->measurement_start,pst->avg_dq_time,pst->dq_count,pst->sflags,q->stats.tot_pkts,q->stats.tot_bytes,q->stats.length,
-	q->stats.len_bytes,q->stats.drops, dequeue_action);
+	q->stats.len_bytes,q->stats.drops,len,dequeue_action);
 
 	/* Turn PIE on when 1/3 of the queue is full */ 
 	if (!(pst->sflags & PIE_ACTIVE) && q->stats.len_bytes >= 
