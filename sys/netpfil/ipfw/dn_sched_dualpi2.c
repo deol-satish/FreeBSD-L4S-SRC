5048e20c82d2978233f8dd1b6e7534e35e09cffb
Deol Satish <164171952+deol-satish@users.noreply.github.com>
2025-04-29 12:51:17 +1000

    Update dn_sched_dualpi2.c

--- sys/netpfil/ipfw/dn_sched_dualpi2.c
+++ sys/netpfil/ipfw/dn_sched_dualpi2.c
@@ -925,7 +925,7 @@ pie_enqueue(struct dualpi2_flow *q, struct mbuf* m, struct dualpi2_si *si)
 		}
 
 
-	printf("dualpi2_ecn_marking-start,%u,%u,%u,%u,%u,%u,%u,%u,%u,%u,%u,%u,%lu,%u,%u,%u,%u,%lu,%lu,%u,%u,%u,%u,%d,end \n",q->queue_type,pprms->qdelay_ref,pprms->tupdate,
+	printf("dualpi2_ecn_marking-start,%u,%u,%u,%u,%u,%u,%u,%u,%u,%u,%u,%u,%lu,%u,%u,%u,%u,%lu,%lu,%u,%u,%u,%lu,%d,end \n",q->queue_type,pprms->qdelay_ref,pprms->tupdate,
 	pprms->max_burst,pprms->max_ecnth,pprms->alpha,pprms->beta,pprms->flags,
 	pst->burst_allowance,pst->drop_prob,pst->current_qdelay,pst->qdelay_old,pst->accu_prob,
 	pst->measurement_start,pst->avg_dq_time,pst->dq_count,pst->sflags,q->stats.tot_pkts,q->stats.tot_bytes,q->stats.length,
